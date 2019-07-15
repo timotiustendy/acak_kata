d237641e3d71cb0d6b4a07eac3714fdf857d9468
Tendy Cahyadi <timotiustendy@ymail.com>
2019-07-15 10:45:45 +0700

    separate question into separate entity + improve encapsulation

--- game.rb
+++ game.rb
@@ -1,55 +1,25 @@
 require './player'
+require './question'
 
 class Game
-    attr_accessor :player, :original_word, :question, :question_set, :finished
+    attr_accessor :player, :current_word, :question_set, :finished
 
     def initialize
         self.player = Player.new
-        self.question_set = fetch_word
-        self.original_word = select_word(self.question_set)
-        self.question = randomize_word(self.original_word)
+        self.question_set = Array.new
+
+        set_questions
+
+        self.current_word = select_word
         self.finished = false
     end
 
-    def select_word(words)
-        random_index = rand(words.length)
-
-        temp = words[random_index]
-        words.delete_at(random_index)
-
-        return temp
-    end
-
     def show_question
-        puts("Tebak kata: #{self.question}")
+        puts("Tebak kata: #{self.current_word.randomized_word}")
     end
 
     def check_answer(answer)
         is_true?(answer) ? correct_answer : wrong_answer
-    end
-
-    def correct_answer
-        self.player.increase_score
-
-        if no_more_question?(self.question_set)
-            puts("Tidak ada pertanyaan tersisa. ANDA MENANG!")
-            self.finished = true
-
-            return
-        end
-
-        self.original_word = select_word(self.question_set)
-        self.question = randomize_word(self.original_word)
-
-        puts("BENAR point anda: #{self.player.score}!")
-    end
-
-    def wrong_answer
-        puts("SALAH! Silakan coba lagi")
-    end
-
-    def is_true?(answer)
-        return answer == self.original_word
     end
 
     def exit?(answer)
@@ -60,11 +30,52 @@ class Game
         return self.finished
     end
 
+    private
+    def correct_answer
+        self.player.increase_score
+
+        if no_more_question?(self.question_set)
+            puts("Tidak ada pertanyaan tersisa. ANDA MENANG!")
+            self.finished = true
+
+            return
+        end
+
+        self.current_word = select_word
+
+        puts("BENAR point anda: #{self.player.score}!")
+    end
+
+    def wrong_answer
+        puts("SALAH! Silakan coba lagi")
+    end
+
+    def select_word
+        random_index = rand(self.question_set.length)
+
+        temp = self.question_set[random_index]
+        self.question_set.delete_at(random_index)
+
+        return temp
+    end
+
+    def is_true?(answer)
+        return answer == self.current_word.original_word
+    end
+
+    def set_questions
+        content = fetch_word
+
+        content.each do |c|
+            new_question = Question.new(c)
+            self.question_set << new_question
+        end
+    end
+
     def no_more_question?(question_set)
         return true if (question_set.length == 0)
     end
 
-    private
     def fetch_word
         content = Array.new
 
@@ -74,40 +85,5 @@ class Game
         end
 
         return content
-    end
-
-    # need optimization there is a better way to do this
-    def randomize_word(word)
-        initial_position = Array.new
-
-        random_position = randomize_char_position(word)
-        result = build_word(word, random_position)
-
-        return result
-    end
-
-    def randomize_char_position(word)
-        random_position = Array.new
-
-        while true do
-            index = rand(word.length)
-            if !random_position.include?(index)
-                random_position << index
-            end
-
-            break if random_position.length == word.length
-        end
-
-        return random_position
-    end
-
-    def build_word(word, positions)
-        result = Array.new(word.length)
-
-        for i in 0...word.length do
-            result[i] = word[positions[i]]
-        end
-
-        return result.join()
     end
 end
---
+++ question.rb
@@ -0,0 +1,48 @@
+class Question
+    attr_accessor :original_word, :randomized_word
+
+    def initialize(word)
+        self.original_word = word
+        self.randomized_word = randomize_word(word)
+    end
+
+    private
+    def randomize_word(word)
+        initial_position = Array.new
+        result = nil
+
+        loop do
+            random_position = randomize_char_position(word)
+            result = build_word(word, random_position)
+
+            break if (result != self.original_word)
+        end
+
+        return result
+    end
+
+    def randomize_char_position(word)
+        random_position = Array.new
+
+        while true do
+            index = rand(word.length)
+            if !random_position.include?(index)
+                random_position << index
+            end
+
+            break if random_position.length == word.length
+        end
+
+        return random_position
+    end
+
+    def build_word(word, positions)
+        result = Array.new(word.length)
+
+        for i in 0...word.length do
+            result[i] = word[positions[i]]
+        end
+
+        return result.join()
+    end
+end
